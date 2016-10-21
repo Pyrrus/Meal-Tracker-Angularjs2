fedb2a0d8fe9202b1f06988b05d2af99a19d7042
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-21 09:33:58 -0700

    add the a sorting for by number then letter

--- app/range.pipe.ts
+++ app/range.pipe.ts
@@ -13,12 +13,27 @@ export class RangePipe implements PipeTransform {
       if (range !== "none") {
         for (var i = 0; i < input.length; i++) {
           if (input[i].calories >= 500 && range === "500") {
-          	console.log("INININININININ over the range");
             rangeArray.push(input[i]);
           } else if (range !== "500" && input[i].calories <= 500) {
           	rangeArray.push(input[i]);
           }
         }
+
+        rangeArray.sort(function(ob1, ob2) {
+        	if (ob1.calories > ob2.calories) {
+		      return 1;
+		    } else if (ob1.calories < ob2.calories) { 
+		      return -1;
+		    }
+
+		    if (ob1.name < ob2.name) { 
+		      return -1;
+		    } else if (ob1.name > ob2.name) {
+		       return 1
+		    } else { 
+		      return 0;
+		    }
+        });
 
         return rangeArray;
       }
