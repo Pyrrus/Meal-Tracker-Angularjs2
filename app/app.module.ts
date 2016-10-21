9881aed36e5fcc015a75921effe6a59baa83fe5c
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-10-20 19:20:30 -0700

    setup files for the angularjs

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { MealListComponent } from './list-meal.component';
+import { EditMealComponent } from './edit-meal.component';
+import { NewMealComponent } from './new-meal.component';
+import { RangePipe } from './Range.pipe'; // import our pipe here
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    MealListComponent,
+    EditMealComponent,
+    NewMealComponent, 
+    RangePipe,
+  ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
